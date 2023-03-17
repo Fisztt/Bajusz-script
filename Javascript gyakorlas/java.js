5caba6c70549ab5ed8b3f06a64b5d650b992e203
Fisztt <121646672+Fisztt@users.noreply.github.com>
2023-03-17 15:00:35 +0100

    Add files via upload

---
+++ Javascript gyakorlas/java.js
@@ -0,0 +1,33 @@
+function mozgas() {
+  let id = null;
+  const elem = document.getElementById("animate");
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
+
+function mozgas() {
+  let id = null;
+  const elem = document.getElementById("animate");
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.right = pos + "px";
+    }
+  }
+}
