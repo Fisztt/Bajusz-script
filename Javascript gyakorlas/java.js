76bdd46573ae9f56c32530756afa856bc6e30a19
Fisztt <121646672+Fisztt@users.noreply.github.com>
2023-03-17 15:24:54 +0100

    Valami

--- Javascript gyakorlas/java.js
+++ Javascript gyakorlas/java.js
@@ -1,6 +1,29 @@
+// Amúgy ez nem müködik,  html-en belül van script //
 
- // Amúgy ez nem müködik, minden html-en belül van script //
+function mutasd() {
+  document.getElementById("id1").style.visibility = "visible";
+  document.getElementById("suprise").style.visibility = "visible";
+}
 
+function eltunes() {
+  document.getElementById("id1").style.visibility = "hidden";
+  document.getElementById("suprise").style.visibility = "hidden";
+}
+function animatemouseover(azam) {
+  azam.innerHTML = "Köszi";
+  azam.style.backgroundColor = "green";
+}
+function animatemouseout(azam2) {
+  azam2.innerHTML = "Vidd rám </br> az egeret";
+  azam2.style.backgroundColor = "red";
+}
+
+function hatter() {
+  document.body.style.backgroundImage = "url('ocean2.jpg')";
+  document.getElementById("id1").style.color = "red";
+  document.getElementById("btn1").style.visibility = "hidden";
+  document.getElementById("suprise").style.visibility = "visible";
+}
 function mozgas() {
   let id = null;
   const elem = document.getElementById("animate");
@@ -17,10 +40,9 @@ function mozgas() {
     }
   }
 }
-
-function mozgas() {
+function mozgas2() {
   let id = null;
-  const elem = document.getElementById("animate");
+  const elem = document.getElementById("animate2");
   let pos = 0;
   clearInterval(id);
   id = setInterval(frame, 5);
